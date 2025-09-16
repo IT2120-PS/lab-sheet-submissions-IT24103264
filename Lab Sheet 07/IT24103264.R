db42edf1756f349bc0c5f2f9f522845762c283bc
Chameesha Pahan <IT24103264@my.sliit.lk>
2025-09-16 13:22:44 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103264.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\Windows\\Desktop\\IT24103264")
+#Question1
+punif(15,min = 0, max = 40, lower.tail = TRUE)
+#Question2
+pexp(2,rate = 0.33, lower.tail = TRUE)
+#Question3
+#part1
+1-pnorm(130,mean = 100, sd=15, lower.tail = TRUE)
+#part2
+qnorm(0.95, mean = 100, sd = 15)
